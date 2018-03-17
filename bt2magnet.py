c1f809115b749eb6fd01052211793fc6ff1f1062
tataki <492241408@qq.com>
2018-03-17 12:31:26 +0800

    修改

--- bt2magnet.py
+++ bt2magnet.py
@@ -1,13 +1,7 @@
-import bencode
-import hashlib
-import base64
-import sys
+import libtorrent as bt
 
 def bt2magnet(f):
-    # 计算meta数据
-    metadata = bencode.bdecode(f)
-    hashcontents = bencode.bencode(metadata['info'])
-    digest = hashlib.sha1(hashcontents).digest()
-    b32hash = base64.b32encode(digest)
-    return 'magnet:?xt=urn:btih:%s' % b32hash
+    info = bt.torrent_info(f)
+    print("magnet:?xt=urn:btih:%s&dn=%s" % (info.info_hash(), info.name()))
+    return "magnet:?xt=urn:btih:%s&dn=%s" % (info.info_hash(), info.name())
 
--- easy_bt.py
+++ easy_bt.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request,json
+from flask import Flask, render_template, request, jsonify
 from bt2magnet import bt2magnet
 from werkzeug.utils import secure_filename
 import os
 
-
 app = Flask(__name__)
-app.config["UP_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)),"static/uploads/")
+app.config["UP_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/uploads/")
 app.debug = True
 
 
@@ -18,14 +17,15 @@ def index():
 def upload():
     if request.method == "POST":
         f = request.files['file']
-        dir = app.config["UP_DIR"]+f.filename
-        if not os.path.exists(app.config["UP_DIR"]):
-            os.makedirs(app.config["UP_DIR"])
-            os.chmod(app.config["UP_DIR"], 777)
-        f.save(dir)
-        magnet = bt2magnet(dir)
-        return magnet
-    return "err"
+        if f is not None:
+            dir = app.config["UP_DIR"] + secure_filename(f.filename)
+            if not os.path.exists(app.config["UP_DIR"]):
+                os.makedirs(app.config["UP_DIR"])
+                os.chmod(app.config["UP_DIR"], 777)
+            f.save(dir)
+            magnet = bt2magnet(dir)
+        return jsonify(result=magnet, status=1)
+    return jsonify(result="", status=10)
 
 
 if __name__ == '__main__':
