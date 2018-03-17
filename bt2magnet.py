14bd434317e36cece0872117133020acfa911f57
tataki <492241408@qq.com>
2018-03-17 13:09:35 +0800

    change

--- bt2magnet.py
+++ bt2magnet.py
@@ -1,7 +1,7 @@
 import libtorrent as bt
 
+
 def bt2magnet(f):
     info = bt.torrent_info(f)
     print("magnet:?xt=urn:btih:%s&dn=%s" % (info.info_hash(), info.name()))
     return "magnet:?xt=urn:btih:%s&dn=%s" % (info.info_hash(), info.name())
-
